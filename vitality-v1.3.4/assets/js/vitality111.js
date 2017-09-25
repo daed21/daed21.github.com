2f561772b2de009391d985ea2322d59809d3c6fa
渠道运营-田之涵 <078020fudan@163.com>
2017-09-25 10:47:07 +0800

    vitality welcome new

---
+++ vitality-v1.3.4/assets/js/vitality111.js
@@ -0,0 +1,26 @@
+/*!
+ * Vitality v1.3.4 (http://themes.startbootstrap.com/vitality-v1.3.4)
+ * Copyright 2013-2015 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+
+(function($) {
+
+
+    $(".testimonials-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: true,
+        autoHeight: true,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        transitionStyle: "backSlide"
+    });
+
+
+
+})(jQuery); // End of use strict
+
+
